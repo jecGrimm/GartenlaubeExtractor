7f3287c93a42b9a235bd07cb658a188585c96c66
Jana <janagrimm22@gmail.com>
2025-04-09 11:19:40 +0200

    FIXED slurm script

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 #SBATCH --comment="Extract text and metadata from of the Gartenlaube from wikisource"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j.grimm@campus.lmu.de
-#SBATCH --chdir=/home/c/janagrimm@141.84.137.23/Desktop/GartenlaubeExtractor
-#SBATCH --output=/home/c/janagrimm@141.84.137.23/Desktop/GartenlaubeExtractor/slurm.%j.%N.out]
+#SBATCH --chdir=/home/j/janagrimm@141.84.137.23/GartenlaubeExtractor
+#SBATCH --output=/home/j/janagrimm@141.84.137.23/GartenlaubeExtractor/slurm.%j.%N.out
 #SBATCH --ntasks=1
 
 python3 -u extract.py
